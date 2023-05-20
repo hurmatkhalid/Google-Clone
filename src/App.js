38076a4b2706a7168ddb31a1bc1fdfb7dda6ad87
hurmat <hurmatkhalid1822@gmail.com>
2023-05-20 16:20:21 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Let's build the clone</h1>
-			{/* Home page*/}
-			<Home />
-			{/* Result page */}
+			<Router>
+				<Routes>
+					<Route path="/search" element={<h1>This is search page</h1>}></Route>
+					<Route path="/" element={<Home />}></Route>
+				</Routes>
+			</Router>
 		</div>
 	);
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './Home.css';
-
+import { Link } from 'react-router-dom';
+import AppsIcon from '@mui/icons-material/Apps';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 function Home() {
 	return (
 		<div className="home">
-			<h1>This is Home page</h1>
 			<div className="home__header">
-				<div className="home__headerLeft"></div>
-				<div className="home__headerRight"></div>
+				<div className="home__headerLeft">
+					{/* for links we will use react router 
+					for avatars we will use materials ui */}
+
+					{/*about link */}
+					<Link to="/about">About</Link>
+					<Link to="/store">Store</Link>
+					{/*store link */}
+				</div>
+				<div className="home__headerRight">
+					{/* gmail link*/}
+					<Link to="/gmail">Gmail</Link>
+					{/* images link*/}
+					<Link to="/images">Images</Link>
+					{/* quick tools icon*/}
+					<AppsIcon />
+					<AccountCircleIcon />
+					{/*avatar  */}
+				</div>
 			</div>
 			<div className="home__body"></div>
 		</div>
