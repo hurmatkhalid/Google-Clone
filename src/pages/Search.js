715ad0a0abd116bb410177c09b4cd24b48553ae9
hurmat <hurmatkhalid1822@gmail.com>
2023-05-22 00:39:11 +0530

    search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,28 +3,44 @@ import './Search.css';
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
 import MicSharpIcon from '@mui/icons-material/MicSharp';
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-function Search() {
+function Search({ hideButtons = false }) {
 	const [input, setInput] = useState(''); //this is state, thats how we write variables
+	const navigate = useNavigate();
 	const search = (e) => {
 		e.preventDefault(); //this so it prevents refresh
-		console.log('Hello');
+		navigate('/search');
 	};
 	return (
 		<form className="search">
-			{' '}
 			{/*we used form instead of div, and then in button do type='submit' it will make your enter key submit the input hehe */}
 			<div className="search__input">
 				<SearchSharpIcon className="search__inputIcon" />
 				<input value={input} onChange={(e) => setInput(e.target.value)} />
 				<MicSharpIcon />
 			</div>
-			<div className="search__buttons">
-				<Button type="submit" onClick={search} variant="outlined">
-					Google Search
-				</Button>
-				<Button variant="outlined">Google Search</Button>
-			</div>
+			{!hideButtons ? (
+				<div className="search__buttons">
+					<Button type="submit" onClick={search} variant="outlined">
+						Google Search
+					</Button>
+					<Button variant="outlined">I'm feeling lucky</Button>
+				</div>
+			) : (
+				<div className="search__buttons">
+					<Button
+						className="search__buttonsHidden"
+						type="submit"
+						onClick={search}
+						variant="outlined">
+						Google Search
+					</Button>
+					<Button className="search__buttonsHidden" variant="outlined">
+						I'm feeling lucky
+					</Button>
+				</div>
+			)}
 		</form>
 	);
 }
